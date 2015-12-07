7acc92dcea05dcb2b4ce78f411cdb66efd83d4f9
WorkingRobot <aleks.margarian1@gmail.com>
2015-12-06 18:55:01 -0800

    :tada: Release v1.1 :tada:
    Added appointments.

--- Calendar.py
+++ Calendar.py
@@ -59,6 +59,14 @@ elif month == 1:
 text = prefix + ' ' + year
 del year, month, day, prefix
 
+'''                      SUN  MON   TUE  WED  THU  FRI  SAT              '''
+appointments = [     [   [],[],[],[],[],[],[]   ],#WEEK 1
+                     [   [],[],[],[],[],[],[]   ],#WEEK 2
+                     [   [],[],[],[],[],[],[]   ],#WEEK 3
+                     [   [],[],[],[],[],[],[]   ],#WEEK 4
+                     [   [],[],[],[],[],[],[]   ] #WEEK 5
+]
+
 # FUNCTIONS   #
 def typetext(font, size, text, x, y, alias, color, upside):
     defined_font = pygame.font.Font(font, size)
@@ -89,18 +97,50 @@ del strDays
 # -------- Main Program Loop -----------
 while not done:
     screen.fill(white)
+    mouse = pygame.mouse.get_pos()
     # --- Main event loop
     for event in pygame.event.get(): # User did something
         if event.type == pygame.QUIT: # If user clicked close
             done = True # Flag that we are done so we exit this loop
         if event.type == pygame.MOUSEBUTTONDOWN:
-            pass
+            for y in range(len(days)):
+                index = [y,None]
+                y = (y) * 75 + 125
+                for x in range(7):
+                    index[1] = x
+                    x = (x) * 100 + 50
+                    if (mouse[0] > x and mouse[0] < x + 100) and (mouse[1] > y and mouse[1] < y + 75):
+                        if pygame.mouse.get_pressed()[0]:
+                            print(index)
+                            inputWrite = input("Appointment Name: ")
+                            if inputWrite:
+                                appointments[index[0]][index[1]].append(inputWrite)
+                            else:
+                                print("You cannot set an appointment with no name.")
+                            del inputWrite
+                        else:
+                            print("Appointments / Events:")
+                            if appointments[index[0]][index[1]]:
+                                for appointment in appointments[index[0]][index[1]]:
+                                    print(appointment)
+                            else:
+                                print("No Appointments / Events set on this date.")
+            del index
+
     pygame.draw.rect(screen,(0,200,0),[50,25,700,50])
     for y in range(len(days)):
         y = (y) * 75 + 125
         for x in range(7):
             x = (x) * 100 + 50
             pygame.draw.rect(screen,black,[x,y,100,75],1)
+    for y in range(len(days)):
+        pos = [(y) * 75 + 125,'']
+        for x in range(7):
+            pos[1] = (x) * 100 + 50
+            #print(pos)
+            if appointments[y][x]:
+                pygame.draw.circle(screen,red,[pos[1]+10,pos[0]+60],3)
+#pygame.draw.rect(screen,black,[x,y,100,75],1)
     for x in range(7):
         x = x * 100 + 50
         pygame.draw.rect(screen,black,[x,75,100,50],1)
